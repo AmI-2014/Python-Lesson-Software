5d7cd3ca7167d207ec5253af7c24d8ff1ff91d55
bonino <dario.bonino@gmail.com>
2014-04-04 11:58:17 +0200

    added language selection to tts module

--- os_metrics.py
+++ os_metrics.py
@@ -3,7 +3,7 @@ Created on Mar 17, 2014
 
 @author: Dario Bonino <dario.bonino@polito.it>
 '''
-import os
+import subprocess
 
 def print_sys_metrics():    
     #get uname data
@@ -19,7 +19,7 @@ def print_sys_metrics():
     print "load_avg:\n \t%f (1min)\n \t%f (5min)\n \t%f (15min)"%(load)
 
     # system-dependent memory info
-    memory = os.popen("cat /proc/meminfo").read()
+    memory = Popen("cat /proc/meminfo").read()
     memory_tuple = memory.split("\n")
     print "Total memory:\n \t%s"%memory_tuple[0].split(":")[1].replace(" ","")
     print "Free memory:\n \t%s"%memory_tuple[1].split(":")[1].replace(" ","")
